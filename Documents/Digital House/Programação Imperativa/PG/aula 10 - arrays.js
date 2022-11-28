96a0187bfc3c6f4dfad6d465fc1c82022b1da2f7
André Paulo <andrericao@hotmail.com>
2022-11-28 12:23:08 -0300

    apanhando do for

--- Documents/Digital House/Programação Imperativa/PG/aula 10 - arrays.js
+++ Documents/Digital House/Programação Imperativa/PG/aula 10 - arrays.js
@@ -1,16 +1,43 @@
+// EXEMPLOS DO EXERCÍCIO
+
+let numbers = [22, 33, 54, 66, 72];
+console.log(numbers[numbers.length]);
+
+let grupoDeAmigos = [ ["Harry", "Ron", "Hermione"], ["Spiderman", "Hulk", "Ironman"], ["Penélope Glamour", "Pierre Nodoyuna","Patán"] ];
+console.log(grupoDeAmigos[1][0]);
+
+
+let str = "uma string qualquer"
+let grupoDeAmigos2 = [ [45, 89, 0], ["Digital", "House", true], ["string", "123","false", 54, true, str] ];
+console.log(grupoDeAmigos2[2][grupoDeAmigos2[2].length - 1]);
+
+
+
+// SOMAR ARRAY 
+
+function soma (array) {
+  //    INÍCIO   /    CONDIÇÃO    / MODIFICADOR
+  for (let i = 0; i < array.length; i ++) {
+    array[i] += array[i]
+  }
+  return array
+}
+console.log('Resultador da função soma');
+console.log(soma(numbers));
+
 //const prompt = require('prompt-sync')()
 /*
 let numbers = [22, 33, 54, 66, 72];
 
 let numbers2 = [];
 
-console.log(numbers[4], numbers[3], numbers[2], numbers[1], numbers[0]);*/
-const vetor_a = [22, 33, 54, 66, 72];//não sabia se declarava ele fora ou dentro da função 
+console.log(numbers[4], numbers[3], numbers[2], numbers[1], numbers[0]);
+//const vetor_a = [22, 33, 54, 66, 72];//não sabia se declarava ele fora ou dentro da função 
 
 function inverterArray(array) {
 
   const n = vetor_a.lenght - 1;
-  const vetor_b = Array(n);// qual a diferença entre array, array() e Array?
+  const vetor_b = array(n);// qual a diferença entre array, array() e Array?
 
   let j = vetor_a.lenght - 1;
   for (item of vetor_a) {
@@ -25,7 +52,7 @@ function inverterArray(array) {
 
 inverterArray(vetor_a); //não sabia como chamar a função
  
-
+*/
 
 /*
 function inverterArray(array) {
@@ -52,3 +79,4 @@ for (int i = 0; i < 5; i++){
 cout << b[i] << " ";
 }
 */
+
